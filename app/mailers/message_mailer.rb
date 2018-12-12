869035d1affbf9cce3fcb552e2600425597d76b8
EarlGenest <charles.genestoux@gmail.com>
2018-12-12 14:40:04 +0100

    pamaetrage action mailer+mailjet

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,10 +1,10 @@
 class MessageMailer < ApplicationMailer
 
-	default from: "charles.genestoux@gmail.com"
+	default from: "soul.kitchen.rennes@gmail.com"
   
   def general_message(contact)
     @contact = contact
-    mail( :to => "admin@example.com", :subject => "You Have a Message From Your Website")
+    mail( :to => "soul.kitchen.rennes@gmail.com", :subject => "You Have a Message From Your Website")
   end
 
 end
