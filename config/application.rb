9e23d40d7a13fb8039518d932d58d7b8834b3da3
Airvanne <erwanb@lilo.org>
2018-12-13 13:29:18 +0100

    trad devise

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module SoulKitchen
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.action_mailer.delivery_method = :mailjet_api
+    config.i18n.default_locale = :fr
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
